0fa795370977bb0211db867ab599254357f3c025
flyyuan <740004544@qq.com>
2022-01-05 21:55:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <h1>Project App ! vue 2 project</h1>
+    <Content />
+    <CompositionApi />
+  </div>
+</template>
+
+<script>
+//import Content from "@v2b/Content"
+export default {
+  components: {
+    Content: () => import('@v2b/Content'),
+    CompositionApi: () => import('@v2b/CompositionApi'),
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.main {
+  img {
+    width: 200px;
+  }
+
+  h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 60px;
+  }
+}
+</style>
